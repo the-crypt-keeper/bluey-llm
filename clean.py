9d29bd1236ee52b128b262f21e0af80a87901342
Mike <mike@cannstandard.om>
2023-06-28 16:22:34 -0400

    cleanest possible input data

--- clean.py
+++ clean.py
@@ -2,11 +2,18 @@ import glob
 
 outlines = []
 
+skips = {'scrape/Rug Island.txt': 25,
+         'scrape/Handstand.txt': -1}
+
 for script in glob.glob('scrape/*.txt'):
     lines = open(script).readlines()
     filtered_lines = []
+    skip_lines = skips.get(script, 5)
 
-    for line in lines[5:]:
+    if skip_lines == -1:
+        continue
+
+    for line in lines[skip_lines:]:
         if line.strip() == '':
             continue
         if line.strip() == 'Bluey episode scripts':
@@ -21,21 +28,29 @@ for script in glob.glob('scrape/*.txt'):
             continue
 
         # remove in need of content warnings
-        if line.find('This article/section is in need of content!') > -1:
+        if line.find('This article/section is in need of content!') > -1 or line.find('You can help by editing this section.') > -1:
+            continue
+        if line.strip().lower() == 'wip' or line.strip().lower() == 'work in progress':
             continue
 
         # remove icons
         if line.find('[[File:') > -1:
             continue
         
-        # remove quotes, they are not consistent
-        line = line.replace('"','')
+        # remove quotes, they are not consistent and trim trailing whitespace
+        line = line.replace('"','').strip()
 
         # if the line does not already start with a speaker, append Scene:
         if line.find(':') == -1:
-            line = 'Scene: '+line 
+            line = 'Scene: '+line.replace('[]','')
+        else:
+            # strip whitespace around the speaker
+            parts = line.split(':')
+            if len(parts) > 2:
+                parts = [ parts[0], ':'.join(parts[1:]) ]
+            line = parts[0].strip()+': '+parts[1].strip()
         
-        filtered_lines.append(line)
+        filtered_lines.append(line+'\n')
 
     if len(filtered_lines) < 16:
         print('SKIP', script, len(lines), len(filtered_lines))
